2cd810d6696f4f63f304e2e28b4aeaeaadf14347
raymetro <jrwynn646@gmail.com>
2019-09-04 14:15:15 +0000

    Make CarRunner class and a toString for Car

--- week4/Car.java
+++ week4/Car.java
@@ -1,1 +1,22 @@
-public class
+import java.awt.Color;
+
+public class Car
+{
+    private String model;
+    private Color color;
+    private int numPassengers;
+    private double amountOfGas;
+
+    public Car()
+    {
+        model = "Mustang";
+        color = Color.YELLOW;
+        numPassengers = 2;
+        amountOfGas = 12.5;
+    }
+
+    public String toString()
+    {
+        return "This is my Mustang";
+    }
+}
