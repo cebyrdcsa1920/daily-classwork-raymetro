3e49979740bc2431f33bad142259955b7f5af65c
raymetro <jrwynn646@gmail.com>
2019-09-20 14:07:27 +0000

    made 3 classes

---
+++ week6/Hellcat.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+
+public class Hellcat
+{
+    private String model;
+    private Color color;
+    private int numPassengers;
+    private double amount of gas;
+    private int top speed
+
+    public Car()
+    {
+        model = "Hellcat";
+        color = Color.RED;
+        numPassengers = 2;
+        TopSpeed = 196
+        amountOfGas = 45.9;
+
+     }
+
+    public Car(String model, Color color, int numPassengers, int TopSpeed, double amountOfGas)
+    {
+        this.model = model;
+        this.color = color;
+        this.numPassengers = numPassengers;
+        this.amountOfGas = amountOfGas;
+}
---
+++ week6/Truck.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+
+public class Truck
+{
+    private String model;
+    private Color color;
+    private int numPassengers;
+    private double amount of gas;
+
+    public Car()
+    {
+        model = "Ford";
+        color = Color.BLACK;
+        numPassengers = 4;
+        amountOfGas = 45.9;
+    }
+
+    public Car(String model, Color color, int numPassengers, double amountOfGas)
+    {
+        this.model = model;
+        this.color = color;
+        this.numPassengers = numPassengers;
+        this.amountOfGas = amountOfGas;
+}
